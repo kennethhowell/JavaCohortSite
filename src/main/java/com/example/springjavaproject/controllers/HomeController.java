dceaa9f4653e38535b8fa08eec036f6c0cfa2b06
kennethhowell <howell.kenneth.a@gmail.com>
2022-08-15 14:31:02 -0500

    scaffolded controller sent variable basic view test complete

---
+++ src/main/java/com/example/springjavaproject/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.springjavaproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/hello")
+    public String helloView(Model model){
+
+        model.addAttribute("testVariable", "String thang");
+
+        return "home";
+
+    }
+
+}
+
+
